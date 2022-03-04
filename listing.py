a48710311de1f740e20e5242c8ac2f64ceaeed2d
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2022-03-03 19:48:08 -0700

    Update listing.py

--- listing.py
+++ listing.py
@@ -1,4 +1,11 @@
 def second_largest(numbers):
+    '''
+    this function finds the second largest number un the list 
+    numbers: 
+        is a list that
+    :return: 
+        returns the index of the second largest value in the list in a variable called second
+    '''
     largest = 0
     second = 0
     for index in numbers:
@@ -9,18 +16,43 @@ def second_largest(numbers):
             second = index
     return second
 def between(numbers,lower,upper):
+    '''
+    between returns all of the values from the list that are inbetween the paramaters (inclusive)
+     numbers: 
+        a list
+     lower: 
+        the lower value 
+    upper: 
+        the upper value
+    :return: 
+    a list with the numbers inbetween the lower and upper paramaters(inbclusive) called bet
+    '''
     bet = []
     for index in numbers:
         if index >=lower and index <=upper:
             bet.append(index)
     return bet
 def no_duplicates(numbers):
+    '''
+    deletes all of the duplicates in the list "numbers"
+     numbers: 
+        is the list that gets modified
+    :return: 
+        a list called 'copy'
+    '''
     copy = []
     for index in numbers:
         if not(index in copy):
             copy.append(index)
     return copy
 def del_duplicate_adjacents(numbers):
+    '''
+    this function only deletes the duplicate numbers that are next to each other in the list 'numbers'
+    :param numbers: 
+        is the list that gets modified
+    :return: 
+     none
+    '''
     index = len(numbers)-1
     while index >0:
         if numbers[index] == numbers[index-1]:
@@ -29,6 +61,13 @@ def del_duplicate_adjacents(numbers):
         index = index - 1
     #return numbers
 def sum_sublists(lists):
+    '''
+    adds tha values off all the sub-0lists together
+    :param lists: 
+        is the 2D list that gets its values added up and put into another list 
+    :return: 
+        the list of sums called "results"
+    '''
     results=[]
     for index in lists:
         count = 0
@@ -37,12 +76,28 @@ def sum_sublists(lists):
         results.append(count)
     return results
 def flatten(lists):
+    '''
+    this function turns the 2D list into a a single list 
+    :param lists: 
+        is the 2d list 
+    :return: 
+     a list of all of the values called "results"
+    '''
     results = []
     for index in lists:
         for endex in index:
             results.append(endex)
     return results
 def is_median(number, target):
+    '''
+    this function findses if the target value is the median
+    :param number: 
+        the list that is used to find the median
+    :param target: 
+        is what the function is trying to see if the median is this value
+    :return: 
+    True or False depending if "target" is equal to the median
+    '''
     index = 0
     half = (len(number)-1)/2
     while index <len(number):
@@ -53,6 +108,13 @@ def is_median(number, target):
     return False
 
 def differences(numbers):
+    '''
+    this function creates a list based off of how the values in the list differ from one another
+    :param numbers: 
+    the list of values
+    :return: 
+    returns a list called "results" where values came from numbers
+    '''
     results = []
     index = 0
     while index < len(numbers)-1:
@@ -61,6 +123,13 @@ def differences(numbers):
         results.append(dif)
     return results
 def odds_or_evens(numbers):
+    '''
+    this function counts the total odd and even values in hte list "numbers" and puts them all into their own respective lists
+    :param numbers: 
+        is the list where the list "odd" and the list "even" gets their names from
+    :return: 
+    which ever list (odd or even) depending on whice one is lager and if they are the same length it returns even 
+    '''
     odd = []
     even = []
     for index in numbers:
@@ -73,6 +142,13 @@ def odds_or_evens(numbers):
     else:
         return even
 def running_total(numbers):
+    '''
+    this function keeps a running total if all the values in a list then adds those values to another list
+    :param numbers: 
+    its the list where all of the values are added up then added to another list 
+    :return: 
+    the list "results" where all of the numbers are added up 
+    '''
     results = []
     count = 0
     for index in numbers:
@@ -80,6 +156,13 @@ def running_total(numbers):
         results.append(count)
     return results
 def comma_separated(numbers):
+    '''
+    this function turns a list into a string with the commas incuded
+    :param numbers: 
+    the list that gets turned into a string 
+    :return: 
+         a string value called "results" and is based of of the list "numbers"
+    '''
     results=""
     results= str(numbers)
     return results
